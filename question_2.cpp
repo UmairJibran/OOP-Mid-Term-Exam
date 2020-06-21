47687e87047d956ac8fa3464819de2a23971037a
Umair Jibran <umairjibran7@gmail.com>
2020-06-21 12:37:35 +0000

    completed till last functionality

--- question_2.cpp
+++ question_2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string> 
 using namespace std;
 
 class Player{
@@ -12,9 +13,60 @@ class Player{
       this->team = team;
       this->average = average;
     }
+
+    void setData(){
+
+    }
+
+    void update(char attribute, string newData){
+      switch(attribute){
+        case 'n':
+        case 'N':{
+          this->name = newData;
+          break;
+        }
+        case 't':
+        case 'T':{
+          this->team = newData;
+          break;
+        }
+        case 'a':
+        case 'A':{
+          this->average = stod(newData);
+          break;
+        }
+      }
+    }
+
+    void displayData(){
+      cout << endl << "Name: " << this->name;
+      cout << endl << "Team: " << this->team;
+      cout << endl << "Average: " << this->average;
+    }
 };
 
 int main(){
-  cout << "Hello 2";
+  string name,team;
+  double avg;
+  cout << endl << "Enter Your Name : ";
+  cin >> name;
+  cout << endl << "Enter Your Team Name : ";
+  cin >> team;
+  cout << endl << "Enter Your Average Score : ";
+  cin >> avg;
+  Player player(name,team,avg);
+  player.displayData();
+  char att;
+  string newValue;
+  cout << endl << "Which Attribute would you like to update"
+       << endl << "A. Average"
+       << endl << "T. Team"
+       << endl << "N. Name" 
+       << endl << ": ";
+  cin >> att;
+  cout << "Enter New Value: ";
+  cin >> newValue;
+  player.update(att, newValue);
+  player.displayData();
   return 0;
 }
