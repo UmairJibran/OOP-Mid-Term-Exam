cffdb764a2abcde10faf07c6370f7a447b514b6e
Umair Jibran <umairjibran7@gmail.com>
2020-06-21 11:27:54 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Check File Named Question-1 For Question 1" << std::endl;
+  std::cout << "Check File Named Question-2 For Question 2" << std::endl;
+}
